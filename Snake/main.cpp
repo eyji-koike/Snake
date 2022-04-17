a5f0ea34f04559f82af6dc7c3e40452993a0405b
Eyji <eyji.koike@gmail.com>
2022-04-17 19:01:07 -0400

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Eyji Koike Cuff on 4/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
